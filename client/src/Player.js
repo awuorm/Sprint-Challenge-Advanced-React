e8b1277d45d9411010dbf6f861b329ef43d09a11
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-04 11:20:59 +0300

    created fetch custom hook

--- client/src/Player.js
+++ client/src/Player.js
@@ -2,16 +2,17 @@ import React,{Component} from "react";
 import styled from "styled-components";
 
 
+
 class Player extends Component {
     constructor(props) {
         super(props);
     }
     render() {
         console.log("players props",this.props.player)
-        return(<div key={this.props.player.id}>
-            <h1>Name: {this.props.player.name} </h1>
-            <h1>Country: {this.props.player.country} </h1>
-            <h1>Searches: {this.props.player.searches} </h1>
+        return(<div style={{border: "2px solid green", margin: "1em"}} key={this.props.player.id}>
+            <h1 style={{fontSize:"1.1em"}}>Name: {this.props.player.name} </h1>
+            <h1 style={{fontSize:"1.1em"}}>Country: {this.props.player.country} </h1>
+            <h1 style={{fontSize:"1.1em"}}>Searches: {this.props.player.searches} </h1>
         </div>)
     }
 }
